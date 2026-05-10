8da92973af3780b97b84d1909b5bdd2446de47a7
Anh Tester <thaian.it15@gmail.com>
2026-05-10 17:42:42 +0700

    Update version dependency

--- src/test/java/com/anhtester/firstpackage/FirstClass.java
+++ src/test/java/com/anhtester/firstpackage/FirstClass.java
@@ -5,26 +5,26 @@ import java.time.LocalDateTime;
 import org.testng.annotations.Test;
 
 public class FirstClass {
-    @Test(groups = {"smoke", "regression"})
-    public void firstTest() {
+    @Test(groups = {"smoke", "regression"}, priority = 1)
+    public void TC_01_firstTest() {
         System.out.println("First test method");
     }
 
-    @Test
-    public void secondTest() {
+    @Test(groups = {"regression"})
+    public void TC_02_secondTest() {
         System.out.println("Second test method");
 
         // Ví dụ trong một phương thức:
         System.out.println("Current time: " + LocalDateTime.now());
     }
 
-    @Test(groups = {"smoke"})
-    public void thirdTest() {
+    @Test(groups = {"smoke"}, priority = 2)
+    public void TC_03_thirdTest() {
         System.out.println("Third test method");
     }
 
     @Test(groups = {"smoke", "regression"})
-    public void fourthTest() {
+    public void TC_04_fourthTest() {
         System.out.println("Fourth test method");
     }
 }
